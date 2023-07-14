6025ae708e85a509d45a2987164cdfcb624c4172
Isaiah Wilson <isaiahrwilson@gmail.com>
2023-07-14 14:31:58 -0500

    Palidrome exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public bool IsPalindrome(string word)
+		{
+			string reversedWord = string.Empty;
+
+			for (int i= word.Length - 1; i >= 0; i--)
+			{
+				reversedWord += word[i];
+			}
+
+			if (word == reversedWord)
+			{
+				return true;
+
+			}
+
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -5,10 +5,21 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hannah", true)]
+        [InlineData("hector", false)]
 
+        public void IsPalidromeTest(string someWord, bool expected)
+        {
+            //Arrange - prep
+            var instance = new WordSmith();
+
+            //Act- call method
+            var actual = instance.IsPalidrome(someWord);
+            //Assert- compare
+            Assert.Equal(expected, actual);
         }
+
     }
 }
